fc560e976102be659014f27c0daac169dac61787
田邉亜季穂 <na@wlan-napt-002.u-aizu.ac.jp>
2019-07-09 15:57:33 +0900

    Initial revision

---
+++ dice.java
@@ -0,0 +1,10 @@
+class dice {
+  public static void main(String[] args) {
+    System.out.println("Rolling dice...");
+    int dice1 = new.java.util.Random().nextint(7) + 1;
+    int dice2 = new.java.util.Random().nextint(7) + 1;
+    int number = dice1 + dice2;
+    System.out.println("Die1 : " + dice1);
+    Syetem.out.println("Die2 : " + dice2);
+  }
+}
